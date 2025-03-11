07e8b90a58948dd2546827740b4f17a5a2ac88eb
hrithikscube <hrithik@scube.me>
2025-03-11 13:55:16 +0530

    cleanup

---
+++ src/pages/test/index.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Test = () => {
+    return (
+        <div className='flex flex-col items-center justify-center w-full h-screen'>Test</div>
+    )
+}
+
+export default Test
